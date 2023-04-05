e97d489a86717f0ee7a22367b83513a2d2341ba8
chasedashiell <dashchax@gmail.com>
2023-04-05 12:05:36 -0500

    whyyyyyy

--- StringListSort.java
+++ StringListSort.java
@@ -1,9 +1,20 @@
 import java.util.ArrayList;
 
-public class StringListSearch {
+public class StringListSort {
 
-    public static int unorderedSearch(ArrayList<String> items, String term) {
-        return 0;
+    public static void selectionSort(ArrayList<String> list){
+        String minValue = list.get(0);
+        int minIndex = 0;
+
+        for (int i = 1; i < list.size(); i++){
+            if (minValue.compareTo(list.get(i)) < 0){
+                String tmp = minValue;
+                minValue = list.get(i);
+                list.set(i, tmp);
+                list.set(minIndex, list.get(i));
+                minIndex++;
+            }
+        }
     }
     
     public static void main(String[] args) {
--- StringListSortTest.java
+++ StringListSortTest.java
@@ -8,6 +8,9 @@ import org.junit.Test;
 public class StringListSortTest {
     @Test
     public void testSelectionSort() {
-        
+        ArrayList<String> input = new ArrayList<>(Arrays.asList("z", "b", "a", "p", "a"));
+        ArrayList<String> expectedOutput = new ArrayList<>(Arrays.asList("a", "a", "b", "P", "z"));
+        StringListSort.selectionSort(input);
+        assertEquals(expectedOutput, input);
     }
 }
